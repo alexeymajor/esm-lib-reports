d8f2ab612dc4195448b104d60252e7354fca5a0c
alexeymajor <alexeymajor@gmail.com>
2022-05-27 16:29:12 +0300

    ref

--- src/main/java/ru/avm/reports/dto/ReportFieldDto.java
+++ src/main/java/ru/avm/reports/dto/ReportFieldDto.java
@@ -3,11 +3,12 @@ package ru.avm.reports.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
 @Value
 @AllArgsConstructor
 @Builder
-
+@Jacksonized
 public class ReportFieldDto {
     Long id;
     String title;
--- src/main/java/ru/avm/reports/dto/ReportResultDto.java
+++ src/main/java/ru/avm/reports/dto/ReportResultDto.java
@@ -3,12 +3,14 @@ package ru.avm.reports.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 
 import java.util.List;
 
 @Value
 @AllArgsConstructor
 @Builder
+@Jacksonized
 public class ReportResultDto {
     List<ReportFieldDto> fields;
     List<Object[]> data;
